16a70bda144313257c6f169b3380cf3746b38f1c
ludvigockerman <ludvig.ockerman@gmail.com>
2026-09-16 17:36:08 +0200

    Added basic chess board

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,37 @@
 #[derive(Clone, Copy)]
-pub enum PieceType{
+pub enum PieceType {
     Pawn,
     Rook,
     Knight,
     Bishop,
     Queen,
-    King
+    King,
 }
 
 #[derive(Clone, Copy)]
-pub struct Pos{
+pub struct Pos {
     pub x: u8,
-    pub y: u8
+    pub y: u8,
 }
 
 #[derive(Clone, Copy)]
 pub struct Piece {
     pub is_white: bool,
-    pub piece_type: PieceType
+    pub piece_type: PieceType,
 }
 
+pub struct Board {
+    pub squares: [Option<Piece>; 64],
+}
+
+impl Board {
+    pub fn new() -> Board {
+        Board {
+            squares: [None; 64],
+        }
+    }
+    pub fn get(&self, pos: Pos) -> Option<Piece> {
+        let index = (pos.y * 8 + pos.x) as usize;
+        self.squares[index]
+    }
+}
